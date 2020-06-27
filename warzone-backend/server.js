4d3e45ba311b8f76dcdd1ea54206126c1c034fed
Derek Stutts <dstutts@redventures.com>
2020-06-27 15:17:04 -0400

    refactors charts and removes unneeded styles

--- warzone-backend/server.js
+++ warzone-backend/server.js
@@ -14,7 +14,6 @@ res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
 next(); 
 });
 
-
 API.login(process.env.COD_EMAIL, process.env.COD_PW).then((res) => {
   console.log(res);
 }).catch((err) => {
@@ -22,19 +21,6 @@ API.login(process.env.COD_EMAIL, process.env.COD_PW).then((res) => {
 });
 
 const players = ['d_stutts', 'parky914', 'waking1987', 'burly1984', 'muskrat_mitch', 'robearpig'];
-
-// const getAllPlayerStats = () => {
-//   players.forEach()
-// }
-
-// app.get('/', (req, res) => {
-//   API.MWBattleData('d_stutts').then(data => {
-//     res.send(data);
-//   }).catch(err => {
-//     console.log('why broken');
-//       console.log(err);
-//   });
-// });
 
 const getPlayerStats = (player) => {
   return API.MWBattleData(player).then(data => {
@@ -47,37 +33,12 @@ const getPlayerStats = (player) => {
   });
 }
 
-// API.MWBattleData('d_stutts').then(data => {
-//   console.log('player data', data);
-//   return data
-// }).catch(err => {
-//   console.log('why broken');
-//     console.log(err);
-// });
-
 app.get('/', (req, res) => {
   const results = Promise.all(players.map(getPlayerStats));
-  console.log('results', results);
   results.then((data) => {
-    console.log('daaata', data);
     res.send(data);
   })
 });
-
-
-
-// route for playstation users
-// app.get('/psn/:gtag', (req, res) => {
-//     let obj = {}
-//     COD_PSN.MWstats(req.params.gtag, COD_PSN.platforms.psn)
-//         .then(output => obj['1'] = output)
-//         .then(axios.get(`https://public-api.tracker.gg/apex/v1/standard/profile/2/${req.params.gtag}`, {headers: {'TRN-Api-Key': `${config.apiKey}`}})
-//                 .then(output => obj['2'] = output.data)
-//                 .then(() => res.send(obj))
-//                 .catch(err => console.log(err)))
-//         .catch(err => console.log(err))
-// })
-
 
 app.listen(port, () => {
     console.log(`Server listening on ${port}`)
--- warzone-frontend/pages/index.js
+++ warzone-frontend/pages/index.js
@@ -2,35 +2,12 @@ import Head from 'next/head';
 import axios from 'axios';
 import Chart from 'chart.js';
 import { useEffect, useState, useRef } from 'react';
+import BarChart from '../components/BarChart';
 import "../styles/styles.scss"
 
 export default function Home() {
 
 const [playerData, setPlayerData] = useState(null);
-
-const getNames = () => {
-  return playerData.map(player => player.playerName);
-}
-
-const getWins = () => {
-  return playerData.map(player => player.br.wins);
-}
-
-const getKd = () => {
-  return playerData.map(player => player.br.kdRatio);
-}
-
-const getGamesPlayed = () => {
-  return playerData.map(player => player.br.gamesPlayed);
-}
-
-const getRevives = () => {
-  return playerData.map(player => player.br.revives);
-}
-
-const getScorePerMinute = () => {
-  return playerData.map(player => player.br.scorePerMinute);
-}
 
 useEffect(() => {
   if (!playerData) {
@@ -43,277 +20,50 @@ useEffect(() => {
   }
 }, []);
 
-
-const winChartRef = useRef(null);
-const kdChartRef = useRef(null);
-const gamesPlayedChartRef = useRef(null);
-const revivesChartRef = useRef(null);
-const SPMChartRef = useRef(null);
-
-useEffect(() => {
-  const myWinChartRef = winChartRef.current.getContext("2d");
-  const myKdChartRef = kdChartRef.current.getContext("2d");
-  const myGamesPlayedChartRef = gamesPlayedChartRef.current.getContext("2d");
-  const myRevivesChartRef = revivesChartRef.current.getContext("2d");
-  const mySPMChartRef = revivesChartRef.current.getContext("2d");
-
-  const { height: graphHeight } = myWinChartRef.canvas;
-
-  let gradientLine = myWinChartRef
-    .createLinearGradient(0, 0, 0, graphHeight);
-      gradientLine.addColorStop(0, "rgb(75, 255, 30, 1.0)");
-      gradientLine.addColorStop(0.5, "rgb(75, 255, 30, 0.15)");
-      gradientLine.addColorStop(1, "rgb(75, 255, 30, 0.05)");
-
-  if (playerData) {
-    new Chart(myWinChartRef, {
-      type: 'line',
-      data: {
-        labels: getNames(),
-        datasets: [
-          {
-            label: 'Wins',
-            data: getWins(),
-            backgroundColor: gradientLine
-          }
-        ]
-      },
-      options: {
-        responsive: true,
-        scales: {
-          xAxes: [{
-              gridLines: {
-                  display: false,
-              }
-          }],
-          yAxes: [{
-              gridLines: {
-                  display: false,
-              }
-          }]
-        },
-        layout: {
-          padding: {
-            left: 50,
-            right: 50,
-            top: 50,
-            bottom: 50
-          }
-        },
-        legend: {
-          labels: {
-            fontColor: '#ffffff',
-            boxWidth: 0
-          }
-        }
-      }
-    });
-    new Chart(myKdChartRef, {
-      type: 'line',
-      data: {
-        labels: getNames(),
-        datasets: [
-          {
-            label: 'Kill / Death Ratio',
-            data: getKd(),
-            backgroundColor: gradientLine
-          }
-        ]
-      },
-      options: {
-        responsive: true,
-        scales: {
-          xAxes: [{
-              gridLines: {
-                display: false,
-              },
-              ticks: {
-                fontColor: '#fff'
-              }
-          }],
-          yAxes: [{
-              gridLines: {
-                  display: false,
-              }
-          }]
-        },
-        layout: {
-          padding: {
-            left: 50,
-            right: 50,
-            top: 50,
-            bottom: 50
-          }
-        },
-        legend: {
-          labels: {
-            fontColor: '#ffffff',
-            boxWidth: 0
-          }
-        }
-      }
-    });
-    new Chart(myGamesPlayedChartRef, {
-      type: 'line',
-      data: {
-        labels: getNames(),
-        datasets: [
-          {
-            label: 'Games Played',
-            data: getGamesPlayed(),
-            backgroundColor: gradientLine
-          }
-        ]
-      },
-      options: {
-        responsive: true,
-        scales: {
-          xAxes: [{
-              gridLines: {
-                  display: false,
-              }
-          }],
-          yAxes: [{
-              gridLines: {
-                  display: false,
-              }
-          }]
-        },
-        layout: {
-          padding: {
-            left: 50,
-            right: 50,
-            top: 50,
-            bottom: 50
-          }
-        },
-        legend: {
-          labels: {
-            fontColor: '#ffffff',
-            boxWidth: 0
-          }
-        }
-      }
-    });
-    new Chart(myRevivesChartRef, {
-      type: 'line',
-      data: {
-        labels: getNames(),
-        datasets: [
-          {
-            label: 'Revives',
-            data: getRevives(),
-            backgroundColor: gradientLine
-          }
-        ]
-      },
-      options: {
-        responsive: true,
-        scales: {
-          xAxes: [{
-              gridLines: {
-                  display: false,
-              }
-          }],
-          yAxes: [{
-              gridLines: {
-                  display: false,
-              }
-          }]
-        },
-        layout: {
-          padding: {
-            left: 50,
-            right: 50,
-            top: 50,
-            bottom: 50
-          }
-        },
-        legend: {
-          labels: {
-            fontColor: '#ffffff',
-            boxWidth: 0
-          }
-        }
-      }
-    });
-    new Chart(mySPMChartRef, {
-      type: 'line',
-      data: {
-        labels: getNames(),
-        datasets: [
-          {
-            label: 'Score Per Minute',
-            data: getScorePerMinute(),
-            backgroundColor: gradientLine
-          }
-        ]
-      },
-      options: {
-        responsive: true,
-        scales: {
-          xAxes: [{
-              gridLines: {
-                  display: false,
-              }
-          }],
-          yAxes: [{
-              gridLines: {
-                  display: false,
-              }
-          }]
-        },
-        layout: {
-          padding: {
-            left: 50,
-            right: 50,
-            top: 50,
-            bottom: 50
-          }
-        },
-        legend: {
-          labels: {
-            fontColor: '#ffffff',
-            boxWidth: 0
-          }
-        }
-      }
-    });
+const charts = [
+  { 
+    label: 'Wins',
+    type: 'bar',
+    stat: 'wins'
+  },
+  { 
+    label: 'Kill / Death Ratio',
+    type: 'bar',
+    stat: 'kdRatio'
+  },
+  { 
+    label: 'Games Played',
+    type: 'bar',
+    stat: 'gamesPlayed'
+  },
+  { 
+    label: 'Revives',
+    type: 'bar',
+    stat: 'revives'
+  },
+  { 
+    label: 'Score Per Minute',
+    type: 'bar',
+    stat: 'scorePerMinute'
   }
-}, [playerData]);
-
-// console.log(playerData);
+];
 
   return (
     <div className="container">
-        {/* <h1>Warzone</h1>
-      {playerData && (
-        playerData.map((player) => {
-        return (
-          <h3 key={player.playerName}>{player.playerName}</h3>
-        )
-      }))} */}
       <div className="charts">
-        <canvas
-          id='winChart'
-          ref={winChartRef}
-        />
-        <canvas
-          id='kdChart'
-          ref={kdChartRef}
-        />
-        <canvas
-          id='gamesPlayedChart'
-          ref={gamesPlayedChartRef}
-        />
-        <canvas
-          id='revivesChart'
-          ref={revivesChartRef}
-        />
-        <canvas
-          id='scorePerMinuteChart'
-          ref={SPMChartRef}
-        />
+        {charts.map((chart) => {
+          const { stat, label, type } = chart;
+          return (
+            <div className="chart">
+              <BarChart
+                label={label}
+                statType={stat}
+                playerData={playerData}
+                type={type}
+              />
+            </div>
+          )
+        })}
       </div>
     </div>
   )
